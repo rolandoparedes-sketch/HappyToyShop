247741898a16576e264163616ae94acbe312c009
rolandoparedes-sketch <rolando.paredes@tecsup.edu.pe>
2026-06-16 15:31:13 -0500

    Cinematic 1
    Añadi una cinematica de inicio ala escena 3D con spline

--- Assets/HappyToyShop/Scripts/CinematicManager.cs
+++ Assets/HappyToyShop/Scripts/CinematicManager.cs
@@ -1,16 +1,59 @@
+using System.Collections;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CinematicManagert : MonoBehaviour
+public class CinematicManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+    public CinemachineCamera introcamera;
+    public CinemachineCamera playerCamera;
+    public Animator fadeAnimator;
+
+    private Vector3 startPos;
+    private bool movedAway = false;
+    private bool finished = false;
+
     void Start()
     {
-        
+        startPos = introcamera.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (finished) return;
+
+        float distance = Vector3.Distance(introcamera.transform.position, startPos);
+
+        if (!movedAway && distance > 10f)
+        {
+            movedAway = true;
+           
+        }
+
+        if (movedAway && distance < 2f)
+        {
+           
+            finished = true;
+            StartCoroutine(FinishIntro());
+        }
+    }
+
+    IEnumerator FinishIntro()
+    {
         
+        fadeAnimator.Play("FadeIn 0");
+
+        yield return new WaitForSeconds(0.02f);
+
+
+       
+        introcamera.Priority = 10;
+        playerCamera.Priority = 50;
+
+     
+
+        fadeAnimator.SetTrigger("TransitionEffectCamera");
+
     }
 }
