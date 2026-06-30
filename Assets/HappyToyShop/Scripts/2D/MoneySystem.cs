cf4316cb472316bed0456299d82af5aaf98e1fc1
rolandoparedes-sketch <rolando.paredes@tecsup.edu.pe>
2026-06-30 12:30:39 -0500

    Sintatico

--- Assets/HappyToyShop/Scripts/2D/MoneySystem.cs
+++ Assets/HappyToyShop/Scripts/2D/MoneySystem.cs
@@ -25,38 +25,38 @@ public class MoneySystem : MonoBehaviour
     [Button]
     public void Buy(int ShelfID, int Amount)
     {
-        if (Amount <= 0)
+        if (Amount <= 0) // 1
         {
-            Debug.Log("You need to buy at least 1 toy");
-            return;
+            Debug.Log("You need to buy at least 1 toy"); //1
+            return; //+1
         }
 
-        var factory = GameManager2D.instance.FactorySystem;
-        var warehouse = GameManager2D.instance.WarehouseSystem;
+        var factory = GameManager2D.instance.FactorySystem; //1
+        var warehouse = GameManager2D.instance.WarehouseSystem; // 1
 
 
-        if (CheckIfThereIsEnoughMoney(ShelfID, Amount))
+        if (CheckIfThereIsEnoughMoney(ShelfID, Amount)) //+1
         {
 
 
-            factory.TakeToy(ShelfID);
+            factory.TakeToy(ShelfID); // +1
 
-            GameManager2D.instance.WarehouseSystem.AddToyToShelf(ShelfID, Amount);
+            GameManager2D.instance.WarehouseSystem.AddToyToShelf(ShelfID, Amount); //+1
         }
 
     }
     public bool CheckIfThereIsEnoughMoney(int ShelfID, int Amount)
     {
-        var factory = GameManager2D.instance.FactorySystem;
+        var factory = GameManager2D.instance.FactorySystem; //1
 
-        ToyData data = factory.TakeToy(ShelfID);
+        ToyData data = factory.TakeToy(ShelfID); //1
        
 
-        if (CurrentMoney < data.FactoryCost)
+        if (CurrentMoney < data.FactoryCost) //1
         {
-            Debug.Log("Try to buy " + Amount + " " + data.EntityName + " : " + data.Description);
-            Debug.Log("Insufficient funds, you need: " + (data.FactoryCost - CurrentMoney) + " to buy");
-            return false;
+            Debug.Log("Try to buy " + Amount + " " + data.EntityName + " : " + data.Description); //1
+            Debug.Log("Insufficient funds, you need: " + (data.FactoryCost - CurrentMoney) + " to buy");//1
+            return false; //1
         }
         else
         {
